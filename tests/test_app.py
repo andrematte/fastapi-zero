0bc37548e4f4a47d3376e78930dc523398006052
Andre Mattos <carlosandrematte@gmail.com>
2025-08-05 19:19:52 -0300

    Aula 01: Criando repositório

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.json() == {'message': 'Hello World!'}
